e9f1c92e325f084675a96d4b071b8c8237320199
赵煜杰 <825407762@qq.com>
2017-05-31 09:53:20 +0800

    1

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo 123;
 docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.8.1-alpine go build -v
 docker build -t go-fs-img-1:v1 .
+
+printf '🍻 %.0s' {1..30}
+echo "end"
