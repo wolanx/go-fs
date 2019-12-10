de4d7bd9d85f130e61e274571758ce96ecd315c1
赵煜杰 <825407762@qq.com>
2019-12-10 17:13:22 +0800

    fix(header) support ali antd option header check

--- src/handle/upload.go
+++ src/handle/upload.go
@@ -22,7 +22,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "OPTIONS" {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-requested-with")
 		return
 	}
 	if r.Method == "POST" {
